a3ce67f8844c3f323cb961726bee91101b8409c3
Jalakandeshwaran <raonejalagandeswaran@gmail.com>
2024-10-07 14:51:02 +0530

    Create material_theme.dart

---
+++ material_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+final ThemeData materialTheme = ThemeData(
+  primarySwatch: Colors.blue,
+  textTheme: TextTheme(
+    bodyText1: TextStyle(fontSize: 18.0),
+  ),
+);
