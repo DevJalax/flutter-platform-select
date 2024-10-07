e370be273382047864e0443495915d907cc219d1
Jalakandeshwaran <raonejalagandeswaran@gmail.com>
2024-10-07 14:50:07 +0530

    Create cupertino_theme.dart

---
+++ cupertino_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+final CupertinoThemeData cupertinoTheme = CupertinoThemeData(
+  primaryColor: CupertinoColors.activeBlue,
+  textTheme: CupertinoTextThemeData(
+    textStyle: TextStyle(fontSize: 18.0),
+  ),
+);
