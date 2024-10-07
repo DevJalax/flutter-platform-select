c7bbcaf988bac0793e17c8d3013ccc359c0240ff
Jalakandeshwaran <raonejalagandeswaran@gmail.com>
2024-10-07 14:47:13 +0530

    Create home_screen_android.dart

---
+++ home_screen_android.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenAndroid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Android Home")),
+      body: Center(child: Text("Hello from Android")),
+    );
+  }
+}
